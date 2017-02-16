b4e6229607d8a74373126dad3707ae653af70fc4
root <root@longwoodgardens.org>
2017-02-16 16:48:20 +0000

    Stylings, file upload size increased

---
+++ sites/default/files/php/twig/bc289e3e_file-link.html.twig_d101a48b0222ef74f417981f7c9d7f53b092a386b2c4cfa5f4565cd001983c3c/ff452c2c122dd82287b04ee0bd8c57a3f6b89759cf95cbae7c14f7bec6d786da.php
@@ -0,0 +1,92 @@
+<?php
+
+/* core/themes/classy/templates/field/file-link.html.twig */
+class __TwigTemplate_c914f02926a167b031bc4d72e5b8032906f4a0915b16ec782930fd420eed64da extends Twig_Template
+{
+    public function __construct(Twig_Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->parent = false;
+
+        $this->blocks = array(
+        );
+    }
+
+    protected function doDisplay(array $context, array $blocks = array())
+    {
+        $__internal_cab2bc5dc2508be7e1990ddda104ffe25db320f45799569595bec86510f68ff5 = $this->env->getExtension("native_profiler");
+        $__internal_cab2bc5dc2508be7e1990ddda104ffe25db320f45799569595bec86510f68ff5->enter($__internal_cab2bc5dc2508be7e1990ddda104ffe25db320f45799569595bec86510f68ff5_prof = new Twig_Profiler_Profile($this->getTemplateName(), "template", "core/themes/classy/templates/field/file-link.html.twig"));
+
+        $tags = array();
+        $filters = array();
+        $functions = array("attach_library" => 14);
+
+        try {
+            $this->env->getExtension('sandbox')->checkSecurity(
+                array(),
+                array(),
+                array('attach_library')
+            );
+        } catch (Twig_Sandbox_SecurityError $e) {
+            $e->setTemplateFile($this->getTemplateName());
+
+            if ($e instanceof Twig_Sandbox_SecurityNotAllowedTagError && isset($tags[$e->getTagName()])) {
+                $e->setTemplateLine($tags[$e->getTagName()]);
+            } elseif ($e instanceof Twig_Sandbox_SecurityNotAllowedFilterError && isset($filters[$e->getFilterName()])) {
+                $e->setTemplateLine($filters[$e->getFilterName()]);
+            } elseif ($e instanceof Twig_Sandbox_SecurityNotAllowedFunctionError && isset($functions[$e->getFunctionName()])) {
+                $e->setTemplateLine($functions[$e->getFunctionName()]);
+            }
+
+            throw $e;
+        }
+
+        // line 14
+        echo $this->env->getExtension('sandbox')->ensureToStringAllowed($this->env->getExtension('drupal_core')->escapeFilter($this->env, $this->env->getExtension('drupal_core')->attachLibrary("classy/file"), "html", null, true));
+        echo "
+<span";
+        // line 15
+        echo $this->env->getExtension('sandbox')->ensureToStringAllowed($this->env->getExtension('drupal_core')->escapeFilter($this->env, (isset($context["attributes"]) ? $context["attributes"] : null), "html", null, true));
+        echo ">";
+        echo $this->env->getExtension('sandbox')->ensureToStringAllowed($this->env->getExtension('drupal_core')->escapeFilter($this->env, (isset($context["icon"]) ? $context["icon"] : null), "html", null, true));
+        echo " ";
+        echo $this->env->getExtension('sandbox')->ensureToStringAllowed($this->env->getExtension('drupal_core')->escapeFilter($this->env, (isset($context["link"]) ? $context["link"] : null), "html", null, true));
+        echo "</span>
+";
+        
+        $__internal_cab2bc5dc2508be7e1990ddda104ffe25db320f45799569595bec86510f68ff5->leave($__internal_cab2bc5dc2508be7e1990ddda104ffe25db320f45799569595bec86510f68ff5_prof);
+
+    }
+
+    public function getTemplateName()
+    {
+        return "core/themes/classy/templates/field/file-link.html.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  50 => 15,  46 => 14,);
+    }
+}
+/* {#*/
+/* /***/
+/*  * @file*/
+/*  * Theme override for a link to a file.*/
+/*  **/
+/*  * Available variables:*/
+/*  * - attributes: The HTML attributes for the containing element.*/
+/*  * - link: A link to the file.*/
+/*  * - icon: The icon image representing the file type.*/
+/*  **/
+/*  * @see template_preprocess_file_link()*/
+/*  *//* */
+/* #}*/
+/* {{ attach_library('classy/file') }}*/
+/* <span{{ attributes }}>{{ icon }} {{ link }}</span>*/
+/* */
