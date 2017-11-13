4e3917393430bd0bb4d76bf9f359fd429b5f7986
root <root@longwoodgardens.org>
2017-11-13 17:42:23 +0000

    Page title fixes

--- themes/lwgvolunteer_style/js/pageTitle.js
+++ themes/lwgvolunteer_style/js/pageTitle.js
@@ -1,6 +1,5 @@
 document.addEventListener('DOMContentLoaded', function() {
 		var title = document.querySelector('#block-pagetitle-2 > div > h1');
 		title.innerText = "Volunteer Hub";
-		title.style.right = "800px";
 });
 
